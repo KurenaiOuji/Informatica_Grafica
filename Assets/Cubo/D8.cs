1d7214d093894b3757e356cb370b2c1cc2a72b7d
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2024-03-07 21:07:10 -0600

    D8
    Listo, no se ve bien la imagen pero ya esta.

--- Assets/Cubo/D8.cs
+++ Assets/Cubo/D8.cs
@@ -23,53 +23,43 @@ public class D8 : MonoBehaviour
 
     void CreateCube()
     {
-        float size = 1f;
         Vector3[] vertices = {
-            new Vector3(0, 0, size), //A, 0
-            new Vector3(-size, 0, 0), //B, 1
-            new Vector3(0, -size, 0), //C, 2
-            new Vector3(size, 0, 0), //E, 3
-            new Vector3(0, size, 0), //F, 4
-            new Vector3(0, 0, -size), //G, 5
+            new Vector3(0, 0, 0), //A, 0
+            new Vector3(1, 0, 0), //B, 1
+            new Vector3(0, 1, 0), //C, 2
+            new Vector3(1, 1, 0), //D, 3
+            new Vector3(1, 1, 0), //E, 4
+            new Vector3(0.5f, 0.5f, -1), //F, 5
+            new Vector3(0.5f, 0.5f, 1), //G, 6
+            new Vector3(0.5f, 0.5f, 1), //H, 7
+            new Vector3(0.5f, 0.5f, 1), //I, 8
+            new Vector3(0.5f, 0.5f, 1), //J, 9
         };
 
         int[] triangles = {
-            0, 1, 2, // front (A, E)
-			1, 5, 2,
-
-            3, 4, 0, // back (B, G)
-			5, 4, 3,
-
-            4, 1,0,// left (C, D)
-			1, 4, 5,
-
-            5, 3, 2,//right (?, F)
-			0, 2, 3
-
-
+            0,1,9,
+            0,8,2,
+            2,7,4,
+            1,3,6,
+            5,2,4,
+            0,2,5,
+            5,1,0,
+            5,3,1,
         };
 
 
 
         Vector2[] uvs = {
-            new Vector2(0, 0.66f),
-            new Vector2(0.25f, 0.66f),
-            new Vector2(0, 0.33f),
-            new Vector2(0.25f, 0.33f),
-
-            new Vector2(0.5f, 0.66f),
-            new Vector2(0.5f, 0.33f),
-            new Vector2(0.75f, 0.66f),
-            new Vector2(0.75f, 0.33f),
-
-            new Vector2(1, 0.66f),
-            new Vector2(1, 0.33f),
-
-            new Vector2(0.25f, 1),
-            new Vector2(0.5f, 1),
-
-            new Vector2(0.25f, 0),
-            new Vector2(0.5f, 0),
+            new Vector2(.75f, 1f),
+            new Vector2(0, 0.75f),
+            new Vector2(5f, 0.75f),
+            new Vector2(1f, 0.75f),
+            new Vector2(0.25f, 0.50f),
+            new Vector2(0.75f, 0.50f),
+            new Vector2(0, 0.25f),
+            new Vector2(0.5f, 0.25f),
+            new Vector2(1f, 0.25f),
+            new Vector2(0.75f, 0),
         };
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
@@ -78,7 +68,7 @@ public class D8 : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        /*mesh.uv = uvs;*/
+        mesh.uv = uvs;
         mesh.Optimize();
         mesh.RecalculateNormals();
     }
