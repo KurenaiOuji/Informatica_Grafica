d515e4162a1458870d68594016c06db5aec81591
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2024-03-07 23:33:13 -0600

    a
    a

--- Assets/Cubo/D8.cs
+++ Assets/Cubo/D8.cs
@@ -27,33 +27,44 @@ public class D8 : MonoBehaviour
             new Vector3(0, 0, 0), //A, 0
 
             new Vector3(1, 0, 0), //B, 1
-            new Vector3(0, 1, 0), //C, 2
-            new Vector3(1, 1, 0), //D, 3
-            new Vector3(1, 1, 0), //E, 4
+            new Vector3(1, 0, 1), //C, 2
 
-            new Vector3(0.5f, 0.5f, -1), //F, 5
+            new Vector3(0, 0, 1), //D, 3
+            new Vector3(0, 0, 1), //E, 4
 
-            new Vector3(0.5f, 0.5f, 1), //G, 6
-            new Vector3(0.5f, 0.5f, 1), //H, 7
-            new Vector3(0.5f, 0.5f, 1), //I, 8
-            new Vector3(0.5f, 0.5f, 1), //J, 9
+            new Vector3(0.5f, -1, .5f), //F, 5
+
+            new Vector3(0.5f, 1, 0.5f), //G, 6
+            new Vector3(0.5f, 1, 0.5f), //H, 7
+            new Vector3(0.5f, 1, 0.5f), //I, 8
+            new Vector3(0.5f, 1, 0.5f), //J, 9
         };
 
         int[] triangles = {
-            0,1,9,
+            /*0,1,9,
             0,8,2,
             2,7,4,
             1,3,6,
             5,2,4,
             0,2,5,
             5,1,0,
-            5,3,1,
+            5,3,1,*/
+
+            7,1,0, //3
+            8,2,1, //2
+            9,3,2, //8 
+            6,0,4, //5
+
+            0,1,5,//1
+            1,2,5,
+            2,3,5,
+            4,0,5,
         };
 
 
 
         Vector2[] uvs = {
-            new Vector2(.75f, 1f),
+            /*new Vector2(.75f, 1f),
             new Vector2(0, 0.75f),
             new Vector2(0, 0.25f),
             new Vector2(0.75f, 0),
@@ -64,8 +75,22 @@ public class D8 : MonoBehaviour
             new Vector2(0.50f, 0.75f),
             new Vector2(0.25f, 0.50f),
             new Vector2(0.75f, 0.50f),
-            new Vector2(0.5f, 0.25f),
-             
+            new Vector2(0.5f, 0.25f),*/
+
+            new Vector2(.5f,.75f),
+
+            new Vector2(.25f,.5f),
+            new Vector2(.5f,.25f),
+
+            new Vector2(1f,.25f),
+            new Vector2(1f,.75f),
+
+            new Vector2(.75f,.5f),
+
+            new Vector2(.75f,1),
+            new Vector2(0,.75f),
+            new Vector2(0,.25f),
+            new Vector2(.75f,0),
         };
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
